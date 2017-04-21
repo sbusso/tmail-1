3d93925cd887b2357e675138fcb436dc647b7023
Stéphane Busso <stephane.busso@gmail.com>
2017-04-21 17:29:34 +0700

    gomify

---
+++ get.sh
@@ -0,0 +1,16 @@
+go get github.com/nsqio/nsq/...
+go get github.com/codegangsta/cli
+go get github.com/codegangsta/negroni
+go get github.com/go-sql-driver/mysql
+go get github.com/jinzhu/gorm
+go get github.com/julienschmidt/httprouter
+go get github.com/kless/osutil/user/crypt/...
+go get github.com/lib/pq
+go get github.com/mattn/go-sqlite3
+go get github.com/nbio/httpcontext
+go get golang.org/x/crypto/bcrypt
+go get golang.org/x/crypto/blowfish
+
+go get github.com/toorop/tmail/cli
+go get github.com/toorop/tmail/core
+go get github.com/toorop/tmail/rest
